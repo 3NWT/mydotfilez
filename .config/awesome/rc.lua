817c8c6d8a9ea91a0eb5c171a410c4642a8c8cde
3NWT <catikkayaerdem@gmail.com>
2023-02-25 02:20:29 +0300

    layout ayrıldı

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -26,35 +26,10 @@ local logout_popup = require("awesome-wm-widgets.logout-popup-widget.logout-popu
 -- Enable hotkeys help widget for VIM and other apps
 -- when client with a matching name is opened:
 require("awful.hotkeys_popup.keys")
+require("errorhandling")
 require("keybindings")
 require("autostart")
 require("uservariables")
-
--- {{{ Error handling
--- Check if awesome encountered an error during startup and fell back to
--- another config (This code will only ever execute for the fallback config)
-if awesome.startup_errors then
-    naughty.notify({ preset = naughty.config.presets.critical,
-                     title = "Oops, there were errors during startup!",
-                     text = awesome.startup_errors })
-end
-
--- Handle runtime errors after startup
-do
-    local in_error = false
-    awesome.connect_signal("debug::error", function (err)
-        -- Make sure we don't go into an endless error loop
-        if in_error then return end
-        in_error = true
-
-        naughty.notify({ preset = naughty.config.presets.critical,
-                         title = "Oops, an error happened!",
-                         text = tostring(err) })
-        in_error = false
-    end)
-end
--- }}}
-
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
 beautiful.init(gears.filesystem.get_configuration_dir() .. "theme.lua")
