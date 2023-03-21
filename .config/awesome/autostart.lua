0edafa4a165b0908ad1c08bbe9a4604832d4a72d
3NWT <catikkayaerdem@gmail.com>
2023-03-21 19:03:47 +0300

    idfk

--- .config/awesome/autostart.lua
+++ .config/awesome/autostart.lua
@@ -3,8 +3,8 @@ local awful = require("awful")
 -- Add apps to autostart here
 local autostart_apps = {
     "setxkbmap tr",
-    "xclip",
     "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1",
+    "xclip",
     "picom",
     "nm-applet",
     "xrandr --output eDP1 --set scaling\\ mode Full",
